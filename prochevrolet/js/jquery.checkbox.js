d0d3f11416383531dc8b75a8c5e83a41a7a59991
Пользователь <>
2017-01-13 13:08:48 +0300

    version

---
+++ prochevrolet/js/jquery.checkbox.js
@@ -0,0 +1,65 @@
+﻿$(function(){
+	
+	$(".niceCheck").each(	
+			function() {
+        dis($(this));
+	});
+	
+	$(".niceCheck").mousedown(
+	/* при клике на чекбоксе меняем его вид и значение */
+	function() {
+	     changeCheck($(this));
+	});
+	
+	$(".niceCheck").each(
+	/* при загрузке страницы нужно проверить какое значение имеет чекбокс и в соответствии с ним выставить вид */
+		function() {
+	
+	     changeCheckStart($(this));
+		});
+});
+
+	function changeCheck(el)
+	/* 
+	    функция смены вида и значения чекбокса
+	    el - span контейнер дял обычного чекбокса
+	    input - чекбокс
+	*/
+
+	{
+
+	     var el = el,
+	          input = el.find("input").eq(0);
+	     if(!input.attr("checked")) {
+	        el.css("background-position","0 -17px");    
+	        input.attr("checked", true)
+	    } else {
+	        el.css("background-position","0 0");    
+	        input.attr("checked", false)
+	    }
+	     return true;
+	}
+	 
+	function changeCheckStart(el)
+	/* 
+	    если установлен атрибут checked, меняем вид чекбокса
+	*/
+	{
+
+	
+	var el = el,
+	        input = el.find("input").eq(0);
+
+	      if(input.attr("checked")) {
+	        el.css("background-position","0 -17px");    
+	        }
+	     return true;
+		 
+	}
+	
+	function dis(el) {
+			if(el.hasClass('disabled')){
+         el.css("background","url(../img/select/checkbox-disabled.png)");
+    }
+	return true;
+	}
